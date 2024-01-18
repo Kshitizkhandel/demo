982fedeaf9eb5311726b1127f7374d6e9eb8f674
Kshitiz Khandelwal <kshitiz.khandelwal16@gmail.com>
2024-01-18 20:43:47 +0530

    initial init

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.title('CampusX')
+st.title('campusx')
+col1,col2=st.columns(2)
+
+with col1:
+    st.image('D:\github_campusx\IMG_20210824_132021.jpg')
+with col2:
+    st.header('Campusx is an online learning platform')    
