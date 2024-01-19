cf6897a072aaca85967a2a95654f8c2a07d7221d
Kshitiz Khandelwal <kshitiz.khandelwal16@gmail.com>
2024-01-19 23:27:57 +0530

    Changed image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ col1,col2=st.columns(2)
 with col1:
     st.image('D:\github_campusx\IMG_20210824_132021.jpg')
 with col2:
-    st.header('Campusx is an online learning platform')    
+    st.header('Campusx is an online learning platform, changing image')    
 st.header('Courses')
 st.subheader('DSMP')
 st.subheader('ML')  
